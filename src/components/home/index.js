967550d0243bff0380d4a92e95f3cebb90312e82
Henrique Custódio <henriquebcustodio@outlook.com>
2022-03-17 11:53:34 -0300

    Styled hero section

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,14 +1,24 @@
-import { Hero, HeroMain } from "./style";
+import { HeroActions, HeroMain, HomeContactButton } from "./style";
+import { FaLinkedin, FaGithub } from "react-icons/fa"
 
 const Home = () => {
   return (
-    <Hero>
+    <section>
       <HeroMain>
         <h1>Hello, I'm Henrique Bonfá.</h1>
-        <p>I’m a front-end developer passionate about technology and music.</p>
-        <p>I work with tools such as HTML, Javascript, CSS and React to transform great designs into real web pages and applications.</p>
+        <p>A front-end developer passionate about software and technology.</p>
+        <p>I work with tools such as React and Angular to transform great designs into interactive web pages and applications.</p>
+        <HeroActions>
+          <a href="https://www.linkedin.com/in/henriquebonfa/">
+            <FaLinkedin />
+          </a>
+          <a href="https://github.com/henriquebcustodio">
+            <FaGithub />
+          </a>
+          <HomeContactButton>Contact</HomeContactButton>
+        </HeroActions>
       </HeroMain>
-    </Hero>
+    </section>
   )
 }
 export default Home;
--- src/shared/layout/index.js
+++ src/shared/layout/index.js
@@ -1,3 +1,4 @@
 export const TABLET_BREAK = '768px';
+export const DESKTOP_BREAK = '1000px';
 export const HEADER_HEIGHT = '4.5rem';
-export const MAX_CONTENT_WIDTH = '1300px'
+export const MAX_CONTENT_WIDTH = '1100px'
