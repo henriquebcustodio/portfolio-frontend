4bb0b084b7cc7f1586e17c82c052853382c0c31b
Henrique Custódio <henriquebcustodio@outlook.com>
2022-03-19 14:18:29 -0300

    Change mobile break

--- src/shared/layout/index.js
+++ src/shared/layout/index.js
@@ -1,5 +1,5 @@
 export const TABLET_BREAK = '768px';
 export const DESKTOP_BREAK = '1000px';
-export const MOBILE_BREAK = '380px';
+export const MOBILE_BREAK = '360px';
 export const HEADER_HEIGHT = '4.5rem';
 export const MAX_CONTENT_WIDTH = '1000px'
