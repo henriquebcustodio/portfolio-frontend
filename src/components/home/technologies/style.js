38357b3fd9abf6ee8c6b907171abb80e387e961e
Henrique Custódio <henriquebcustodio@outlook.com>
2022-03-19 14:13:54 -0300

    Layout corrections

--- src/components/home/technologies/style.js
+++ src/components/home/technologies/style.js
@@ -1,15 +1,20 @@
 import styled from "styled-components";
-import { TABLET_BREAK } from "../../../shared/layout";
+import { MOBILE_BREAK, TABLET_BREAK } from "../../../shared/layout";
 
 export const TechnologiesContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
   justify-items: center;
   row-gap: 2rem;
   width: 100%;
   overflow-x: hidden;
 
+  @media screen and (min-width: ${MOBILE_BREAK}) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
   @media screen and (min-width: ${TABLET_BREAK}) {
     grid-template-columns: repeat(6, 1fr);
+    justify-content: space-between;
   }
 `
